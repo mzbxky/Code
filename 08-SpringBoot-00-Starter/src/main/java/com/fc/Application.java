4d70345bdf2e97f7372cbc17ab45bfe5a1be656c
mzb <1376055937@qq.com>
2022-04-26 13:18:49 +0800

    SpringBoot入门

---
+++ 08-SpringBoot-00-Starter/src/main/java/com/fc/Application.java
@@ -0,0 +1,15 @@
+package com.fc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//SpringBoot启动类
+//必须放在Controller同包路径下
+
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
