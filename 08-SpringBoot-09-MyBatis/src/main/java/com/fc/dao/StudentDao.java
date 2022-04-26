d9f973a049ea2064c5ad1f628e94314bc352435f
mzb <1376055937@qq.com>
2022-04-26 13:33:01 +0800

    SpringBoot连接数据库

---
+++ 08-SpringBoot-09-MyBatis/src/main/java/com/fc/dao/StudentDao.java
@@ -0,0 +1,11 @@
+package com.fc.dao;
+
+import com.fc.entity.Student;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+//@Mapper
+@Repository
+public interface StudentDao {
+    List<Student> findAll();
+}
